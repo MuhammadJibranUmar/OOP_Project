9988d4c0c90140219728ff49e9d41088f26882c9
Muhammad Jibran Umar <85576672+MuhammadJibranUmar@users.noreply.github.com>
2024-06-09 09:40:07 +0500

    functions added for the registration of courses

--- project.cpp
+++ project.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 const int MAX = 100;
 
+class Course;
+class Student;
+class Faculty;
+//Date class to store date of birth
 class Date
 {
 public:
@@ -35,6 +39,7 @@ public:
         cout << month << "/" << day << "/" << year << endl;
     }
 
+//function to calculate age using ctime library
     void calculateAge()
     {
         time_t now = time(0);
@@ -65,7 +70,7 @@ private:
     int year;
 };
 
-// person class
+// person class to store common attributes of student and faculty
 class Person
 {
 public:
@@ -122,6 +127,7 @@ protected:
     string email;
 };
 
+// student class to store student information
 class Student : public Person
 {
 public:
@@ -144,15 +150,57 @@ public:
         cout << "GPA: " << gpa << endl;
     }
 
-    
+    void registerCourse(Course c)
+    {
+        if (totalCourses == MAX)
+        {
+            cout << "You have already registered maximum courses" << endl;
+            return;
+        }
+        registeredCourses[totalCourses] = &c;
+        totalCourses++;
+
+        c.registerStudent(*this);
+    }
+
+    void displayCourses()
+    {
+        for (int i = 0; i < totalCourses; i++)
+        {
+            registeredCourses[i]->getData();
+        }
+    }
+
+    void displayResult()
+    {
+        for (int i = 0; i < totalCourses; i++)
+        {
+            registeredCourses[i]->displayResult();
+        }
+    }
+
+    void calculateGPA()
+    {
+        double totalCredits = 0;
+        double totalGradePoints = 0;
+        for (int i = 0; i < totalCourses; i++)
+        {
+            totalCredits += registeredCourses[i]->creditHours;
+            totalGradePoints += registeredCourses[i]->result.gradePoints * registeredCourses[i]->creditHours;
+        }
+        gpa = totalGradePoints / totalCredits;
+    }
 
 private:
     int studentID;
     static int totalStudents;
     int registrationNo;
     double gpa;
+    int totalCourses;
+    Course* registeredCourses[MAX];
 };
 
+// faculty class to store faculty information
 class Faculty : public Person
 {
 public:
@@ -174,11 +222,83 @@ public:
         Person::printPerson();
     }
 
+    void assignCourse(Course c)
+    {
+        if (totalCourses == MAX)
+        {
+            cout << "You have already assigned maximum courses" << endl;
+            return;
+        }
+        courses[totalCourses] = &c;
+        totalCourses++;
+
+        c.registerFaculty(*this);
+    }
+
+    void displayCourses()
+    {
+        for (int i = 0; i < totalCourses; i++)
+        {
+            courses[i]->getData();
+        }
+    }
+
+    void displayStudents()
+    {
+        for (int i = 0; i < totalCourses; i++)
+        {
+            courses[i]->displayStudents();
+        }
+    }
+
+    void enterMarks(){
+        while(true){
+            cout << "1. Enter Marks" << endl;
+            cout << "2. Exit" << endl;
+            cout << "Enter your choice: ";
+            int choice;
+            cin >> choice;
+            cin.ignore();
+            if(choice == 1){
+                cout << "Enter Course ID: ";
+                int id;
+                cin >> id;
+                cin.ignore();
+                for(int i = 0; i < totalCourses; i++){
+                    if(courses[i]->courseID == id){
+                        courses[i]->setMarks();
+                        break;
+                    }
+                }
+            }
+            else if(choice == 2){
+                break;
+            }
+            else{
+                cout << "Invalid choice" << endl;
+            }
+        }
+    }
+
+    void facultyMenu()
+    {
+        while (true)
+        {
+            cout << "1. Display Courses" << endl;
+            cout << "2. Display Students" << endl;
+            cout << "3. Enter Marks" << endl;
+            
+        }
+    }
 private:
     int facultyID;
     int registrationNo;
     static int totalFaculty;
+    Course* courses[MAX];
+    int totalCourses;
 };
+
+// result class to store student result
 class Result
 {
 public:
@@ -259,38 +379,47 @@ public:
         if (totalMarks >= 84.5 && totalMarks <= 100)
         {
             grade = "A+";
+            gradePoints = 4.0;
         }
         else if (totalMarks >= 79.5 && totalMarks < 84.5)
         {
             grade = "A";
+            gradePoints = 3.7;
         }
         else if (totalMarks >= 75.5 && totalMarks < 79.5)
         {
             grade = "B+";
+            gradePoints = 3.4;
         }
         else if (totalMarks >= 69.5 && totalMarks < 75.5)
         {
             grade = "B";
+            gradePoints = 3.0;
         }
         else if (totalMarks >= 64.5 && totalMarks < 69.5)
         {
             grade = "B-";
+            gradePoints = 2.5;
         }
         else if (totalMarks >= 59.5 && totalMarks < 64.5)
         {
             grade = "C+";
+            gradePoints = 2.0;
         }
         else if (totalMarks >= 54.5 && totalMarks < 59.5)
         {
             grade = "C";
+            gradePoints = 1.5;
         }
         else if (totalMarks >= 49.5 && totalMarks < 54.5)
         {
             grade = "D";
+            gradePoints = 1.0;
         }
         else
         {
             grade = "F";
+            gradePoints = 0.0;
         }
     }
 
@@ -304,6 +433,8 @@ public:
         return totalMarks;
     }
 
+    float gradePoints;
+
 private:
     float mids;
     float sessional;
@@ -315,6 +446,7 @@ private:
     string grade;
 };
 
+// course class to store course information
 class Course
 {
 public:
@@ -418,17 +550,46 @@ public:
         cout << "Grade: " << result.getGrade() << endl;
     }
 
+    void registerStudent(Student s)
+    {
+        if (totalStudents == MAX)
+        {
+            cout << "Course is already full" << endl;
+            return;
+        }
+        students[totalStudents] = &s;
+        totalStudents++;
+    }
+    
+    void displayStudents()
+    {
+        for (int i = 0; i < totalStudents; i++)
+        {
+            students[i]->printPerson();
+        }
+    }
+
+    void registerFaculty(Faculty f)
+    {
+        faculty = &f;
+        registered = true;
+    }
+
+    Result result;
+    int creditHours;
+    int courseID;
 
 private:
-    int courseID;
     static int totalCourses;
     bool registered;
     string courseTitle;
-    int creditHours;
     int totalMarks;
-    Result result;
+    int totalStudents;
+    Student* students[MAX];
+    Faculty* faculty;
 };
 
+// department class to store department information
 class Department
 {
 public:
@@ -547,6 +708,7 @@ private:
     Course courses[MAX];
 };
 
+// University class to store university information
 class University
 {
 public:
@@ -618,11 +780,13 @@ private:
     Department departments[MAX];
 };
 
+// static variables initialization
 int Student::totalStudents = 0;
 int Faculty::totalFaculty = 0;
 int Course::totalCourses = 0;
 int Department::totalDepartments = 0;
 
+// function to login
 bool login(){
     string username, password;
     cout << "Enter username: ";
@@ -651,6 +815,7 @@ bool login(){
     return found;
 }
 
+// function to register user
 void registerUser(){
     string username, password;
     cout << "Enter username: ";
@@ -662,9 +827,11 @@ void registerUser(){
     cout << "User registered successfully" << endl;
 }
 
+// main function
 int main()
 {
     cout << "Welcome to University Information System" << endl;
+    // login and register
     while(true){
         cout << "1. Login" << endl;
         cout << "2. Register" << endl;
@@ -691,6 +858,7 @@ int main()
         }
     }
 
+// object of University class
     University Alpha;
 
     while (true)
